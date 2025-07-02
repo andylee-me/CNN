45500419cd2de1c5fbd0881963a06208ab803d23
andylee-me <60034043+andylee-me@users.noreply.github.com>
2025-07-02 18:39:02 +0800

    Update predict.py

--- cat_dog_classifier/predict.py
+++ cat_dog_classifier/predict.py
@@ -3,6 +3,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # 圖片路徑與參數
 val_dir = 'file/kaggle_cats_vs_dogs_f/val'
+train_dir = 'file/kaggle_cats_vs_dogs_f/train'
 img_size = (128, 128)
 batch_size = 32
 
@@ -17,7 +18,18 @@ val_generator = val_datagen.flow_from_directory(
     batch_size=batch_size,
     class_mode='binary'
 )
+train_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=img_size,
+    batch_size=batch_size,
+    class_mode='binary'
+)
 
 # 評估模型
 loss, acc = model.evaluate(val_generator)
-print(f'驗證結果：loss = {loss:.4f}, accuracy = {acc:.4f}')
+print(f'驗證結果：val_loss = {loss:.4f}, val_accuracy = {acc:.4f}')
+
+
+loss, acc = model.evaluate(train_generator)
+print(f'驗證結果：train_loss = {loss:.4f}, train_accuracy = {acc:.4f}')
