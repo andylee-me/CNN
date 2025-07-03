d030d9e3c9b84580d4a01b5caff73395e1c951d8
andylee-me <60034043+andylee-me@users.noreply.github.com>
2025-07-03 21:27:13 +0800

    Update train_cnn.py

--- cat_dog_classifier/train_cnn.py
+++ cat_dog_classifier/train_cnn.py
@@ -5,6 +5,11 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import layers, models, callbacks
 from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 # 資料夾路徑
